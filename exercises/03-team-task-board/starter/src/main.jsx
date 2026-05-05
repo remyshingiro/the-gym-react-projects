1026af46883a71cd5628f67ebbb919113ddfd34b
Remy <shingiroremy303@gmail.com>
2026-05-05 22:08:42 +0200

    members default routes

--- exercises/03-team-task-board/starter/src/main.jsx
+++ exercises/03-team-task-board/starter/src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import TaskProvider from './context/TaskContext.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <TaskProvider>
+    <BrowserRouter>
     <App/>
+      <Routes>
+        <Route path='/alice' element/>
+        <Route path='/bob' element/>
+        <Route path='/carol' element/>
+        <Route path='/david' element/>
+        
+      </Routes>
+    </BrowserRouter>
+    
   </TaskProvider>
 )
