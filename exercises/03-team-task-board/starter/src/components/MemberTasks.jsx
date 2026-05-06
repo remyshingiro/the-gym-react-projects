30cf321840abc22dad5196c415b5b35ac6cda83a
Remy <shingiroremy303@gmail.com>
2026-05-06 21:36:44 +0200

    ji

--- exercises/03-team-task-board/starter/src/components/MemberTasks.jsx
+++ exercises/03-team-task-board/starter/src/components/MemberTasks.jsx
@@ -17,17 +17,18 @@ export default function Member() {
   );
 
   return (
-    <div>
-      <h2>
+    <div className="items-center bg-slate-100 w-screen h-screen  text-center">
+      <h1 className="font-bold pt-9 text-xl">Page displaying member tasks</h1>
+      <h2 className="font-bold">
         Tasks for {member ? member.name : "Member not found"}
       </h2>
       {memberTasks.length === 0 ? (
-        <p>No tasks assigned.</p>
+        <p>No tasks assigned</p>
       ) : (
-        <div>
+        <div className="bg-white rounded-4xl p-3">
           {memberTasks.map(task => (
             <div key={task.id}>
-              <p>{task.title}</p>
+              <p className="text-gray-500">{task.title}</p>
               <p>
                 Priority: {task.priority} | Status: {task.status}
               </p>
